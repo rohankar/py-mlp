d08f42be6c1c98f53afbabc53af25504ae18c5f1
Rohan Kar <rohan@getpostman.com>
2016-08-03 23:58:00 +0530

    first commit

---
+++ classification.py
@@ -0,0 +1,11 @@
+# Package imports
+import matplotlib.pyplot as plt
+import numpy as np
+import sklearn
+import sklearn.datasets
+import sklearn.linear_model
+import matplotlib
+
+# Display plots inline and change default figure size
+%matplotlib inline
+matplotlib.rcParams['figure.figsize'] = (10.0, 8.0)
