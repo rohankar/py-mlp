119d684a143e33c0574a1f9c82bfcc6d5ef51ec3
Rohan Kar <rohankar@live.com>
2016-09-05 22:36:09 +0530

    Update classification.py

--- classification.py
+++ classification.py
@@ -1,10 +1,11 @@
 # Package imports
-import matplotlib.pyplot as plt
-import numpy as np
-import sklearn
+
 import sklearn.datasets
 import sklearn.linear_model
 import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import sklearn
 
 # Display plots inline and change default figure size
 %matplotlib inline
