19a0279bc3a8acc2444e3e8d910d4e3dd5367a41
Rohan Kar <rohankar@live.com>
2016-11-13 07:12:50 +0530

    Update classification.py

--- classification.py
+++ classification.py
@@ -8,5 +8,5 @@ import numpy as np
 import sklearn
 
 # Display plots inline and change default figure size
-%matplotlib inline
+#%matplotlib inline
 matplotlib.rcParams['figure.figsize'] = (10.0, 8.0)
