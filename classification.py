15af450ffa360e80198ac9c9baf2b2c2be6c74c7
Rohan Kar <rohankar@live.com>
2016-11-13 17:33:40 +0530

    MVP: simple nn classification

--- classification.py
+++ classification.py
@@ -1,12 +1,45 @@
-# Package imports
-
-import sklearn.datasets
-import sklearn.linear_model
-import matplotlib
-import matplotlib.pyplot as plt
 import numpy as np
-import sklearn
+from sklearn import datasets, linear_model
+import matplotlib.pyplot as plt
 
-# Display plots inline and change default figure size
-#%matplotlib inline
-matplotlib.rcParams['figure.figsize'] = (10.0, 8.0)
+
+def generate_data():
+    np.random.seed(0)
+    X, y = datasets.make_moons(200, noise=0.20)
+    return X, y
+
+
+def visualize(X, y, clf):
+    plot_decision_boundary(lambda x: clf.predict(x), X, y)
+    plt.title("Logistic Regression")
+
+
+def plot_decision_boundary(pred_func, X, y):
+    # Set min and max values
+    x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+    y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+    h = 0.01
+    # Generate a grid of points with distance h between them
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    Z = pred_func(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    # Plot the contour and training examples
+    plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Spectral)
+    plt.show()
+
+
+def classify(X, y):
+    clf = linear_model.LogisticRegressionCV()
+    clf.fit(X, y)
+    return clf
+
+
+def main():
+    X, y = generate_data()
+    clf = classify(X, y)
+    visualize(X, y, clf)
+
+
+if __name__ == "__main__":
+    main()
